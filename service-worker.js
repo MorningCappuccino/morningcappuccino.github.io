a177c81763934ed1499364d7a6dad1179ff7309b
MorningCappuccino <knockknockwakeup@gmail.com>
2022-05-31 18:54:31 +0400

    build v2: fix 3

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2e2fd78a3b79d2df4471e8da6d56247.js"
+  "/precache-manifest.208549a7faef9dd9f68bcbb4e2874524.js"
 );
 
 self.addEventListener('message', (event) => {
